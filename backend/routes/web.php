128fae42a1106b056f3e13b8630b3791e980d7a3
mhar <romar.bucad@airlinemetrics.com>
2026-04-23 23:01:52 +0800

    test

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,4 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', function(){
+    return "HELLO WORLD";
+});
+
+Route::get('/dashboard', function () {
+    return inertia('welcome');
+});
