03e599b3906707eba0d00255dc6959bbf6c6a75c
mhar <romar.bucad@airlinemetrics.com>
2026-04-23 20:36:51 +0800

    redis

--- backend/resources/js/app.tsx
+++ backend/resources/js/app.tsx
@@ -1,10 +1,19 @@
-import { createInertiaApp } from '@inertiajs/react';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createInertiaApp } from '@inertiajs/react'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.jsx`,
+            import.meta.glob('./pages/**/*.jsx')
+        ),
+
     progress: {
         color: '#4B5563',
     },
-});
+})
